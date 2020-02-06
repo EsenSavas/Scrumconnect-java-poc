e9f1843aa4ea6368994363a65936549aa03e0f71
esengul gungor <esengulsavas@gmail.com>
2020-02-06 22:49:45 +0000

    feat: swagger file is changed

--- src/main/java/com/makeitconsulting/scrumconnectapipoc/configuration/SwaggerConfig.java
+++ src/main/java/com/makeitconsulting/scrumconnectapipoc/configuration/SwaggerConfig.java
@@ -1,15 +1,22 @@
 package com.makeitconsulting.scrumconnectapipoc.configuration;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
 
 @Configuration
 @EnableSwagger2
@@ -17,11 +24,13 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(metadata());
+                .select()//
+                .apis(RequestHandlerSelectors.any())//
+                .paths(Predicates.not(PathSelectors.regex("/error")))//
+                .build()//
+                .apiInfo(metadata())//
+                .useDefaultResponseMessages(false)//
+                .genericModelSubstitutes(Optional.class);
 
     }
 
@@ -32,7 +41,7 @@ public class SwaggerConfig {
                         "This is a sample customer data saving  service.")
                 .version("1.0.0")//
                 .license("UNLICENSED")
-                .contact(new Contact(null, null, "contact@makeit-consulting.co.uk"))
+                .contact(new Contact("Esen Gungor", null, "esengulsavas@gmail.com"))
                 .build();
     }
 }
